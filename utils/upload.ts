fc0e7b35fb48f5faa03452f405da41b019fdb9c8
alltaro <112899939+alltaro@users.noreply.github.com>
2025-03-24 18:24:29 +0000

    Trying another path

--- utils/upload.ts
+++ utils/upload.ts
@@ -2,8 +2,8 @@ import { writeFile, mkdir, readdir } from "fs/promises";
 import { join } from "path";
 
 export async function uploadImage(file: Buffer, voieId: string) {
-  const dirPath = join(process.cwd(), "./public/uploads", voieId);
-  
+  const dirPath = join("./public/uploads", voieId);
+
   // Vérifie si le dossier existe, sinon le crée
   await mkdir(dirPath, { recursive: true });
 
@@ -19,7 +19,7 @@ export async function getImagesForVoie(voieId: string) {
   const dirPath = join(process.cwd(), "./public/uploads", voieId);
   try {
     const files = await readdir(dirPath);
-    return files.map(file => `/uploads/${voieId}/${file}`);
+    return files.map((file) => `/uploads/${voieId}/${file}`);
   } catch (error) {
     console.error("Erreur lors de la récupération des images :", error);
     return []; // Retourne une liste vide si aucune image n'est trouvée
