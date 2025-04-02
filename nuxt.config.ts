a9585dcadd90ff4fe46443019a9be0cca9234a15
alltaro <romeomath07@gmail.com>
2025-04-03 00:35:15 +0200

    modified Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
   ssr: true,
   runtimeConfig: {
     aut: {
       mongoUri: "mongodb://192.168.1.33:27017/Escalade",
-      mongoSecret:
-        "6404f35f37147bca8b50c17e52425a70af0e4b4c7a47942046b305b07a2aac6a",
-      mongoUser:'alttaro',
-      mongoPassword:'YwIcvjVaWiQgttiC'
+      mongoSecret: "6404f35f37147bca8b50c17e52425a70af0e4b4c7a47942046b305b07a2aac6a",
+      mongoUser: "alttaro",
+      mongoPassword: "YwIcvjVaWiQgttiC",
     },
   },
   compatibilityDate: "2024-04-03",
   modules: ["@nuxt/ui", "@nuxtjs/color-mode"],
   colorMode: {
-    classSuffix: "", 
-  },   
-}); 
- 
+    classSuffix: "",
+  },
+};
